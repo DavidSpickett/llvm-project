0ffe3d64a3d05c3574dddafc79c75cb01a0dcbe8
David Spickett <david.spickett@linaro.org>
2025-07-11 10:44:21 +0000

    manual testing changes

--- llvm/utils/release/github-upload-release.py
+++ llvm/utils/release/github-upload-release.py
@@ -160,16 +160,16 @@ if args.user:
     if not args.user_token:
         print("--user-token option required when --user is used")
         sys.exit(1)
-    # Validate that this user is allowed to modify releases.
-    user = gh.get_user(args.user)
-    team = (
-        github.Github(args.user_token)
-        .get_organization("llvm")
-        .get_team_by_slug("llvm-release-managers")
-    )
-    if not team.has_in_members(user):
-        print("User {} is not a allowed to modify releases".format(args.user))
-        sys.exit(1)
+    ## Validate that this user is allowed to modify releases.
+    #user = gh.get_user(args.user)
+    #team = (
+    #    github.Github(args.user_token)
+    #    .get_organization("llvm")
+    #    .get_team_by_slug("llvm-release-managers")
+    #)
+    #if not team.has_in_members(user):
+    #    print("User {} is not a allowed to modify releases".format(args.user))
+    #    sys.exit(1)
 elif args.command == "check-permissions":
     print("--user option required for check-permissions")
     sys.exit(1)
